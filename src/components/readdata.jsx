2030470052928724e0f765581018378991ec02cf
jelegummykub <jelegummy.jrs@gmail.com>
2024-10-12 23:18:01 +0700

    almost success

--- src/components/readdata.jsx
+++ src/components/readdata.jsx
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from 'chart.js';
 import cornerstone, { enable } from 'cornerstone-core';
 import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 import dicomParser from 'dicom-parser';
 import Papa from 'papaparse';
 import React, { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 function Readdata() {
 
@@ -12,6 +16,7 @@ function Readdata() {
     const [imageId, setImageId] = useState('')
     const [csvData, setCsvData] = useState([])
     const [openFolders, setOpenFolders] = useState({})
+    const [chartData, setChartData] = useState({})
 
     useEffect(() => {
         cornerstoneWADOImageLoader.external.cornerstone = cornerstone
@@ -113,21 +118,109 @@ function Readdata() {
             </ul>
         )
     }
+
+    useEffect(() => {
+        if (csvData.length > 0) {
+            const newChartData = {};
+            const headers = Object.keys(csvData[0]);
+            // console.log(headers)
+
+            headers.forEach(header => {
+                const values = csvData.map(row => row[header]);
+                const numericValues = values.filter(value => !isNaN(parseFloat(value)) && isFinite(value))
+                console.log(values.length)
+                // console.log(!isNumeric)
+                if (values.length - 1 > 10) {
+                    if (!numericValues.length && values.length) {
+                        newChartData[header] = {
+                            type: 'count',
+                            totalCount: values.length - 1
+                        }
+                    } else {
+                        const counts = numericValues.reduce((acc, value) => {
+                            acc[value] = (acc[value] || 0) + 1;
+                            return acc;
+                        }, {});
+
+                        newChartData[header] = {
+                            type: 'chart',
+                            labels: Object.keys(counts),
+                            datasets: [{
+                                label: header,
+                                data: Object.values(counts),
+                                backgroundColor: 'rgba(75, 192, 192, 0.6)',
+                            }]
+                        };
+                    }
+                } else if (values.length - 1 < 10 && values.length - 1 > 0) {
+                    newChartData[header] = {
+                        type: 'count',
+                        totalCount: values.length - 1
+                    }
+                }
+
+            });
+
+            setChartData(newChartData);
+        }
+    }, [csvData]);
+
+
+
     const renderCSVData = () => {
         if (csvData.length === 0) return null;
 
+        const options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: true,
+                },
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                }
+            }
+        };
+
         return (
             <div>
-                <h2>CSV Data</h2>
-                <table className="min-w-full">
+                <h2 className="text-2xl font-bold mb-4">CSV Data</h2>
+                <table className="min-w-full border-collapse table-fixed">
                     <thead>
                         <tr>
                             {Object.keys(csvData[0]).map((header, index) => (
-                                <th key={index} className="border px-4 py-2">{header}</th>
+                                <th key={index} className="border px-4 py-2 bg-gray-100">{header}</th>
                             ))}
                         </tr>
                     </thead>
                     <tbody>
+                        <tr className="h-32 sticky top-0 bg-white z-10">
+                            {Object.keys(csvData[0]).map((header, index) => (
+                                <td key={index} className="border px-4 py-2">
+                                    {chartData[header]?.type === 'count' ? (
+                                        <div className="h-32 flex items-center justify-center">
+                                            <span className="text-1xl font-bold">
+                                                {chartData[header].totalCount} total values
+                                            </span>
+                                        </div>
+                                    ) : chartData[header]?.type === 'chart' ? (
+                                        <div className="h-32">
+                                            <Bar options={options} data={chartData[header]} />
+                                        </div>
+                                    ) : (
+                                        <div className="h-32 flex items-center justify-center">
+                                            <span className="text-lg">Data column</span>
+                                        </div>
+                                    )}
+                                </td>
+                            ))}
+                        </tr>
                         {csvData.map((row, rowIndex) => (
                             <tr key={rowIndex}>
                                 {Object.values(row).map((cell, cellIndex) => (
@@ -167,13 +260,13 @@ function Readdata() {
     };
 
     const isPrintable = (str) => {
-        return /^[\x20-\x7E]*$/.test(str);
+        return typeof str === 'string' && /^[\x20-\x7E]*$/.test(str);
     };
 
     return (
         <div>
             <div className='flex'>
-                
+
 
                 <div className='w-[83%] p-4 mr-10'>
                     <div className="border border-gray-300 p-4 mb-4">
@@ -189,9 +282,14 @@ function Readdata() {
                     )}
 
                     {csvData.length > 0 && (
-                        <div className="border border-gray-300 p-4 mb-4">
-                            {renderCSVData()}
-                        </div>
+                        <>
+                            <div className="border border-gray-300 p-4 mb-4">
+                                <div style={{ maxWidth: '100%', overflowX: 'auto' }}>
+                                    {renderCSVData()}
+                                </div>
+                            </div>
+                        </>
+
                     )}
                 </div>
                 <div className='w-1/3 p-4'>
